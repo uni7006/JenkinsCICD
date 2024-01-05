693b975e3cbf34f028ebf7f94a84f8deb0109bbc
uni7006 <85192229+uni7006@users.noreply.github.com>
2024-01-05 20:39:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "Jenkins" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-024f768332f080c5e" 
 }
 variable "versioning" {
     type        = bool
