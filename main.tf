f401ad0c19a3df26e3fb406dab8b273124a7f184
uni7006 <85192229+uni7006@users.noreply.github.com>
2024-01-05 20:16:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,12 +42,3 @@ resource "aws_instance" "myFirstInstance" {
     Name = var.tag_name
   }
 }
-
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
-  }
-}
