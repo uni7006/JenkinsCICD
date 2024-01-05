9ad09f9ff9d4406a0d04f9a4a3c95fa2bfc5388e
uni7006 <85192229+uni7006@users.noreply.github.com>
2024-01-06 00:03:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-s3-bucket-for-tf-state2"
     key = "mys3/terraform.state"
     region = "eu-central-1"
-    dynamodb_table = "my-tf-dynamo-db-table"
+    dynamodb_table = "my-tf-lock"
   }
 }
